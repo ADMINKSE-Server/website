0d02b63351364bb209b8af66375fe25e3eceb829
Kruemmelspalter <mika.thomas@freenet.de>
2020-08-25 10:39:32 +0200

    changed script.js

--- src/script.js
+++ src/script.js
@@ -1,22 +1,27 @@
 function play() {
     document.getElementById('play-box').animate([
         {
-            opacity: 0
+            opacity: 0,
+            'pointer-events':'none'
         },
         {
-            opacity: 1
+            opacity: 1,
+            'pointer-events':'all'
         }
-    ], 2000);
+    ], 1000);
     document.getElementById('play-box').style.opacity=1;
 }
 
 function closePlay() {
     document.getElementById('play-box').animate([
         {
-            opacity: 1
+            opacity: 1,
+            'pointer-events':'all'
         },
         {
-            opacity: 0
+            opacity: 0,
+            'pointer-events':'none'
         }
-    ]);
+    ], 1000);
+    document.getElementById('play-box').style.opacity=0;
 }
